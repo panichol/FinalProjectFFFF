50efdf13be637aa9ea8872470d6561c6cccf319c
Jennifer Ryan & Bradley Bond <jryan@mymail.mines.edu>
2015-11-21 13:16:58 -0700

    Renamed test package to gameTests.

--- src/playerGameStateTests.java
+++
@@ -1,5 +0,0 @@
-import junit.framework.TestCase;
-
-public class playerGameStateTests extends TestCase {
-
-}
--- src/questionAnswerTests.java
+++
@@ -1,5 +0,0 @@
-import junit.framework.TestCase;
-
-public class questionAnswerTests extends TestCase {
-
-}
