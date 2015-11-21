d8e2694148b7af82c4a863ee70def56faab97189
Jennifer Ryan & Bradley Bond <jryan@mymail.mines.edu>
2015-11-21 14:03:19 -0700

    Added question test class and question class.

--- src/gameTests/QuestionAnswerTests.java
+++ src/gameTests/QuestionAnswerTests.java
@@ -3,6 +3,9 @@ import junit.framework.TestCase;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import gamePlay.Fraction;
+import gamePlay.Question;
+
 import org.junit.BeforeClass;
 
 public class QuestionAnswerTests extends TestCase {
@@ -13,8 +16,18 @@ public class QuestionAnswerTests extends TestCase {
 	
 	@Test
 	public void answerHandling(){
-		AnswerMC answer = new AnswerMC();
-		assertEquals(, 5);
+		Fraction answer = new Fraction(1, 2);
+		Fraction false1 = new Fraction(3, 4);
+		Fraction false2 = new Fraction(3, 4);
+		Fraction false3 = new Fraction(3, 4);
+		Question q1 = new Question(answer, false1, false2, false3);
+		
+		Fraction userInputFalse = new Fraction(3, 4);
+		Fraction userInputTrue = new Fraction(1, 2);
+		
+		assertTrue (q1.isCorrect(userInputTrue));		//checks that if user selects answer 
+		assertFalse(q1.isCorrect(userInputFalse));
+		
 		
 	}
 	
