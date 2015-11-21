3ff25a2bbaa0b78e2a2ec6ecb46ab1da7e9cf723
Adam Nadolsky <anadolsk@mines.edu>
2015-11-21 14:00:07 -0700

    added question result tests

---
+++ src/gameTests/Player.java
@@ -0,0 +1,12 @@
+package gameTests;
+
+public class Player {
+private int location;
+private int livesRemaining;
+
+	public Player() {
+		location = 0;
+		livesRemaining = 5;
+	}
+
+}
--- src/gameTests/PlayerGameStateTests.java
+++ src/gameTests/PlayerGameStateTests.java
@@ -1,6 +1,33 @@
 package gameTests;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class PlayerGameStateTests extends TestCase {
-
+private static GameEngine engine;
+private final int TEST_RUNS=200;
+	
+	@BeforeClass
+	public void setUp() {
+		engine = new GameEngine();
+	}
+	
+	@Test
+	public void testQuestionResult(){
+		for (int i=0; i < TEST_RUNS; i++) {
+			int preLocation = engine.getPlayer().getLocation();
+			int preQuestions = engine.getQuestionsLeft();
+			if (engine.askQuestion()) {
+				assertEquals(preLocation + 1, engine.getPlayer().getLocation());
+				assertEquals(preQuestions - 1, engine.getPlayer().getQuestionsLeft();
+			}
+			else {
+				assertEquals(preLocation, engine.getPlayer().getLocation());
+				assertEquals(preQuestions - 1, engine.getPlayer().getQuestionsLeft();
+			}
+		
+		}
+	}
+	
 }
