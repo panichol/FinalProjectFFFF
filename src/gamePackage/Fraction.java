dbe96c7eb4f08a1f681d364b00f7cd672587acf3
Patrick Nichols <Panichol@mymail.mines.edu>
2015-11-21 13:28:19 -0700

    Added skeleton Fraction Class

---
+++ src/gamePackage/Fraction.java
@@ -0,0 +1,21 @@
+package gamePackage;
+
+public class Fraction {
+	private int numerator;
+	private int denominator;
+	public int getNumerator(){
+		return numerator;
+	}
+	public void setNumerator(int numerator){
+		this.numerator = numerator;
+	}
+	public int getDenominator(){
+		return denominator;
+	}
+	public void setDenominator(int denominator){
+		this.denominator = denominator;
+	}
+	public void reduce(){
+		
+	}	
+}
