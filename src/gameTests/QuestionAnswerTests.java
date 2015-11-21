365792db45948c7e6a5aa7e3758ce7aae7c7dac4
Jennifer Ryan & Bradley Bond <jryan@mymail.mines.edu>
2015-11-21 14:19:58 -0700

    Finished Question Answer Tests

--- src/gameTests/QuestionAnswerTests.java
+++ src/gameTests/QuestionAnswerTests.java
@@ -4,29 +4,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import gamePlay.Fraction;
+import gamePlay.GameEngine;
 import gamePlay.Question;
 
 import org.junit.BeforeClass;
 
 public class QuestionAnswerTests extends TestCase {
+	GameEngine newGame;
+	
 	@BeforeClass
 	public void setUp() {
-		
+		newGame = new GameEngine();
 	}
 	
 	@Test
 	public void answerHandling(){
+		
+		
 		Fraction answer = new Fraction(1, 2);
 		Fraction false1 = new Fraction(3, 4);
 		Fraction false2 = new Fraction(3, 4);
 		Fraction false3 = new Fraction(3, 4);
 		Question q1 = new Question(answer, false1, false2, false3);
-		
-		Fraction userInputFalse = new Fraction(3, 4);
-		Fraction userInputTrue = new Fraction(1, 2);
-		
-		assertTrue (q1.isCorrect(userInputTrue));		//checks that if user selects answer 
-		assertFalse(q1.isCorrect(userInputFalse));
+				
+		assertTrue (q1.isCorrect(newGame.getPlayerAnswer()));		//checks that if user selects answer 
+		assertFalse(q1.isCorrect(newGame.getPlayerAnswer()));
 		
 		
 	}
