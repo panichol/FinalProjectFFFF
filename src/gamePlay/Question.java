8bf08e3e90b2b4491bbd37f1ed9270af1e64cdc9
Jennifer Ryan & Bradley Bond <jryan@mymail.mines.edu>
2015-11-24 11:39:30 -0700

    Working on file I/O in Question class.

--- src/gamePlay/Question.java
+++ src/gamePlay/Question.java
@@ -1,6 +1,8 @@
 package gamePlay;
 
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Question {
 	Fraction correctAnswer;
@@ -20,6 +22,44 @@ public class Question {
 		this.falseAnswer1 = falseAnswer1;
 		this.falseAnswer2 = falseAnswer2;
 		this.falseAnswer3  = falseAnswer3;
+	}
+	
+	public void loadQuestionFile(String fileName){
+		FileReader reader;
+		try {
+			reader = new FileReader(fileName);
+			Scanner in = new Scanner(reader);
+			int numQuestions = Integer.parseInt(in.nextLine());
+			for (int i = 0; i < numQuestions; i++){
+				if (in.hasNextLine()){
+					String a = in.nextLine();
+					
+					a = in.nextLine();
+					a = in.nextLine();
+					a = in.nextLine();
+					a = in.nextLine();
+
+				}
+				else{
+					//throw exception
+					System.out.println("error reading input file");
+				}
+			}
+				//first line is string question
+				
+				if(test.length != 3)
+				{
+					throw new BadConfigFormatException("Improper format for key, Incorrect number of elements found on a line");
+				}
+				rooms.put(test[0].charAt(0), test[1]);
+				if(!test[2].equals("Card") && !test[2].equals("Other"))
+				{
+					throw new BadConfigFormatException("Improper type for a room");
+				}
+			}
+		} catch (FileNotFoundException e) {
+			throw new BadConfigFormatException(e.getMessage());
+		}
 	}
 	public Fraction getCorrectAnswer() {
 		return correctAnswer;
