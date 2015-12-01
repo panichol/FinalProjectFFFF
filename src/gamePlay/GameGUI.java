18f03c6158de2f5c26a4e3a4403ca2bef62cd432
Alan Champion <alanchampion@gmail.com>
2015-12-01 13:22:09 -0700

    Added the painting and scaling of the player and background.

--- src/gamePlay/GameGUI.java
+++ src/gamePlay/GameGUI.java
@@ -17,7 +17,7 @@ import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 
 public class GameGUI extends JFrame {
-	private JPanel graphicsPanel;
+	private ImagePanel graphicsPanel;
 	private JPanel questionPanel;
 	private JPanel playerStatus;
 	private JTextField timeDisp;
@@ -26,8 +26,8 @@ public class GameGUI extends JFrame {
 	private ArrayList<JRadioButton> buttons;
 	
 	//Image stuff
-	Image playerSprite;
-	Image background;
+	private Image playerSprite;
+	private Image background;
 
 	public GameGUI() {
 		setSize(600,800);
@@ -46,8 +46,10 @@ public class GameGUI extends JFrame {
 		livesDisp.setEnabled(false);
 		
 		setLayout(new GridLayout(0,1));
-		graphicsPanel = createGraphicsPanel();
+		graphicsPanel = new ImagePanel();
+		graphicsPanel.updateCurrentRock(0);
 		add(graphicsPanel);
+		
 		JPanel bottom = new JPanel();
 		bottom.setLayout(new GridLayout(1,0));
 		playerStatus = createPlayerStatusPanel();
@@ -56,41 +58,81 @@ public class GameGUI extends JFrame {
 		bottom.add(questionPanel);
 		add(bottom);
 	}
-
-	private Image getImage(String pathName) {
-		URL url = getClass().getResource(pathName);
-		Image image = Toolkit.getDefaultToolkit().getImage(url);
-		return image;
-	}
-
-	private ImagePanel createGraphicsPanel() {
-		ImagePanel graphicsPanel = new ImagePanel();
-		return graphicsPanel;
-	}
 	
 	public class ImagePanel extends JPanel{
+		private int playerX = 0;
+		private int playerY = 0;
+		private MediaTracker tracker;
+		private static final int PADDING = 20;
+		private int boardWidth;
+		private int boardHeight;
+		private int playerWidth;
+		private int playerHeight;
+		
 		public ImagePanel() {
-				MediaTracker tracker = new MediaTracker(this);
-				Image player = getImage("/images/player.png");
-				Image back = getImage("/images/board.png");
+				tracker = new MediaTracker(this);
+				Image player = getImage("/images/player.png",1);
+				Image back = getImage("/images/board.png",0);
 				
-				tracker.addImage(background, 0);
-				try {
-					tracker.waitForID(0);
-				} catch (InterruptedException e) {  return; }
-				tracker.addImage(playerSprite, 1);
-				try {
-					tracker.waitForID(0);
-				} catch (InterruptedException e) {  return; }
-				
-				background = back.getScaledInstance(1600, 1000, Image.SCALE_FAST);
+				background = back.getScaledInstance(160, 100, Image.SCALE_FAST);
 				playerSprite = player.getScaledInstance(110, 170,  Image.SCALE_FAST);
 		}
 
+		@Override
 		public void paintComponent(Graphics g) {
-			int PADDING = 20;
-			g.drawImage(background, PADDING, PADDING, 1600, 1000, null);
-			g.drawImage(playerSprite, 0, 0, 110, 170, null);
+			boardWidth = this.getWidth()-2*PADDING;
+			boardHeight = this.getHeight()-2*PADDING;
+			playerWidth = boardWidth*11/160;
+			playerHeight = boardHeight*17/100;
+			
+			g.drawImage(background, PADDING, PADDING, boardWidth, boardHeight, null);
+			//System.out.println(boardWidth*11/160 + ", " + boardHeight);
+			g.drawImage(playerSprite, playerX, playerY, playerWidth, playerHeight, null);
+		}
+		
+		/**
+		 * Used if needing to actively change the pixel location of the player.
+		 * It is recommended to use the updateCurrentRock so that the scaling for different window sizes can take affect. 
+		 * 
+		 * @param x X pixel location
+		 * @param y Y pixel location
+		 */
+		public void updatePlayerLocation(int x, int y) {
+			playerX = x;
+			playerY = y;
+		}
+		
+		/**
+		 * Used to change the player location based off the current rock. 
+		 * This will scale for window size. 
+		 * 
+		 * @param rockNumber The number rock the player is on. 0 and 11 for the banks if wanted. 
+		 */
+		public void updateCurrentRock(int rockNumber) {
+			switch (rockNumber) { //TODO add the switch functionality. 
+			case 0: playerX = PADDING;
+			playerY = PADDING;
+			break;
+			default: break;
+			}
+			repaint();
+		}
+		
+		/**
+		 * Used to load a image from a given path. 
+		 * 
+		 * @param pathName The location of the image in the file tree.
+		 * @param id The order you want the images to be added to the Media Tracker. 
+		 * @return the image.
+		 */
+		private Image getImage(String pathName, int id) {
+			URL url = getClass().getResource(pathName);
+			Image image = Toolkit.getDefaultToolkit().getImage(url);
+			tracker.addImage(image, id); //TODO ensure that this will always allow for the images to be displayed. 
+			try {
+				tracker.waitForID(id);
+			} catch (InterruptedException e) {  return image; }
+			return image;
 		}
 	}
 
